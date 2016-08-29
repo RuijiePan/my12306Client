0ced8123bf38b88be32dff1e86c3a5a5c10e7593
wu <270949894@qq.com>
2016-08-29 15:04:20 +0800

    Signed-off-by: wu <270949894@qq.com> kkkkk

--- src/main/java/com/blog/controller/UserController.java
+++ src/main/java/com/blog/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController extends BaseController{
 	
 	@RequestMapping("/test")
 	public void test(HttpServletRequest request,HttpServletResponse response) throws IOException{
-		System.out.println("========/test=========");
+		System.out.println("========/test======上传试试===");
 		response.getWriter().write("test");
 	}
 }
