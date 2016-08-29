c7412bd5b7b37d3af432a704525a248d53cd4206
wu <270949894@qq.com>
2016-08-29 15:23:32 +0800

    new

--- src/main/java/com/blog/controller/UserController.java
+++ src/main/java/com/blog/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController extends BaseController{
 	
 	@RequestMapping("/test")
 	public void test(HttpServletRequest request,HttpServletResponse response) throws IOException{
-		System.out.println("========/test=121212===");
+		System.out.println("========/test=new===");
 		response.getWriter().write("test");
 	}
 }
